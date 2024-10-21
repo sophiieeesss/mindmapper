9436fb9d8fcd25c9b30b8f2727003bc90e745003
sophiieeesss <sophiehharris@gmail.com>
2024-10-20 22:00:09 -0700

    Update 04drag.js

--- 04drag.js
+++ 04drag.js
@@ -1,33 +1,34 @@
-// Enable drag and drop for the header and phone number
-function makeDraggable(element) {
-    let isDragging = false;
-    let shiftX, shiftY;
+const draggables = document.querySelectorAll('.draggable');
 
-    element.onmousedown = function(event) {
-        isDragging = true;
-        shiftX = event.clientX - element.getBoundingClientRect().left;
-        shiftY = event.clientY - element.getBoundingClientRect().top;
+draggables.forEach(draggable => {
+    draggable.addEventListener('mousedown', function (e) {
+        const element = e.target;
+        let shiftX = e.clientX - element.getBoundingClientRect().left;
+        let shiftY = e.clientY - element.getBoundingClientRect().top;
 
-        document.onmousemove = function(event) {
-            if (isDragging) {
-                element.style.left = event.pageX - shiftX + 'px';
-                element.style.top = event.pageY - shiftY + 'px';
-            }
+        element.style.position = 'absolute';
+        element.style.zIndex = 1000;
+
+        function moveAt(pageX, pageY) {
+            element.style.left = pageX - shiftX + 'px';
+            element.style.top = pageY - shiftY + 'px';
+        }
+
+        moveAt(e.pageX, e.pageY);
+
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        element.onmouseup = function () {
+            document.removeEventListener('mousemove', onMouseMove);
+            element.onmouseup = null;
         };
+    });
 
-        document.onmouseup = function() {
-            isDragging = false;
-            document.onmousemove = null;
-            document.onmouseup = null;
-        };
-    };
-
-    element.ondragstart = function() {
+    draggable.ondragstart = function () {
         return false;
     };
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    makeDraggable(document.getElementById('mainHeader'));
-    makeDraggable(document.getElementById('phone'));
 });
