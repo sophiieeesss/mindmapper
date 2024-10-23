c7c69a89671c343e710a79362fcb3a1fa98d3c9c
sophiieeesss <sophiehharris@gmail.com>
2024-10-22 18:31:51 -0700

    Update z05drag.js

--- z05drag.js
+++ z05drag.js
@@ -1,25 +1,31 @@
-document.querySelectorAll('.draggable').forEach(item => {
-    item.onmousedown = function(event) {
-        let shiftX = event.clientX - item.getBoundingClientRect().left;
-        let shiftY = event.clientY - item.getBoundingClientRect().top;
+let isDragging = false;
+let currentElement = null;
+let offsetX = 0;
+let offsetY = 0;
 
-        item.style.position = 'absolute';
-        item.style.zIndex = 1000;
+function makeDraggable(element) {
+    element.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        currentElement = element;
+        offsetX = e.clientX - currentElement.getBoundingClientRect().left;
+        offsetY = e.clientY - currentElement.getBoundingClientRect().top;
+    });
 
-        function moveAt(pageX, pageY) {
-            item.style.left = pageX - shiftX + 'px';
-            item.style.top = pageY - shiftY + 'px';
-        }
+    element.addEventListener('mouseup', () => {
+        isDragging = false;
+        currentElement = null;
+    });
+}
 
-        function onMouseMove(event) {
-            moveAt(event.pageX, event.pageY);
-        }
-
-        document.addEventListener('mousemove', onMouseMove);
-
-        item.onmouseup = function() {
-            document.removeEventListener('mousemove', onMouseMove);
-            item.onmouseup = null;
-        };
-    };
+document.addEventListener('mousemove', (e) => {
+    if (isDragging && currentElement) {
+        currentElement.style.position = 'absolute';
+        currentElement.style.left = `${e.clientX - offsetX}px`;
+        currentElement.style.top = `${e.clientY - offsetY}px`;
+    }
 });
+
+// Apply dragging to elements
+document.querySelectorAll('.draggable').forEach(makeDraggable);
+document.getElementById("mainHeader").style.cursor = "move";
+document.getElementById("phoneNumber").style.cursor = "move";
