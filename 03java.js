6fb400f59d7267c8762666b5077b858c4f71d419
sophiieeesss <sophiehharris@gmail.com>
2024-10-22 17:27:20 -0700

    Update 03java.js

--- 03java.js
+++ 03java.js
@@ -1,12 +1,11 @@
-const correctPassword = "secretpassword"; // Set your password
+const password = "secretpassword";
 
 function checkPassword() {
-    const input = document.getElementById('passwordInput').value;
-    const errorMessage = document.getElementById('errorMessage');
-    if (input === correctPassword) {
+    const inputPassword = document.getElementById('passwordInput').value;
+    if (inputPassword === password) {
         document.getElementById('login').style.display = 'none';
         document.getElementById('content').style.display = 'block';
     } else {
-        errorMessage.innerText = "Incorrect password. Please try again.";
+        document.getElementById('errorMessage').innerText = 'Incorrect password, try again!';
     }
 }
